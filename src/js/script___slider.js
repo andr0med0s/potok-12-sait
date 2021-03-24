6ccbc93c278b3fb6fdeae89dfa61b7293482c7b6
andr0med0s <andromedoz@gmail.com>
2021-03-24 12:54:36 +0500

    another slider assembly

--- src/js/script___slider.js
+++ src/js/script___slider.js
@@ -1,52 +1,17 @@
 $(document).ready(function() {
-    $('.slider').slick({
-        arrows: true,
-        dots: true,
-        adaptiveHeight: false,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        speed: 1000,
-        easing: 'ease',
-        infinite: true,
-        // initialSlide: 0,
-        // autoplay: false,
-        // autoplaySpeed: 1500,
-        // pausedOnFocus: false,
-        // pausedOnHover: false,
-        // pausedOnDotHover: false,
-        draggable: true,
-        swipe: true,
-        touchThreshold: 10,
-        touchMove: true,
-        waitForAnimate: false,
-        centerMode: true,
-        rows: 1,
-        slidesPerRow: 1,
-        focusOnSelect: true,
-        asNavFor: '.sliderbig',
-    });
-    $('.sliderbig').slick({
+    $('.production-slider_top').slick({
         arrows: false,
-        fade: true,
-        asNavFor: '.slider',
         slidesToShow: 1,
         slidesToScroll: 1,
+        asNavFor: '.production-slider_bottom'
     });
+    $('.production-slider_bottom').slick({
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<div class="slider-arrow slider-arrow_prod slider-arrow_left"><div>',
+        nextArrow: '<div class="slider-arrow slider-arrow_prod slider-arrow_right"><div>',
+        asNavFor: '.production-slider_top'
+    });
+
 });
-
-
-// $('.sliderbig').slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows: false,
-//     fade: true,
-//     asNavFor: '.slider'
-// });
-// $('.slider').slick({
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     asNavFor: '.sliderbig',
-//     dots: true,
-//     centerMode: true,
-//     focusOnSelect: true
-// });
