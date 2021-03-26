034e39bd32e7aa9f0552b084313007bf34d0c984
andr0med0s <andromedoz@gmail.com>
2021-03-26 17:18:26 +0500

    fix file

--- src/js/script___slider.js
+++ src/js/script___slider.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
-            $('.production-slider_top').slick({
-                arrows: false,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                asNavFor: '.production-slider_bottom'
-            });
-            $('.production-slider_bottom').slick({
-                arrows: true,
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                prevArrow: '<div class="slider-arrow slider-arrow_prod slider-arrow_left"><div>',
-                nextArrow: '<div class="slider-arrow slider-arrow_prod slider-arrow_right"><div>',
-                asNavFor: '.production-slider_top'
-            });
+    $('.production-slider_top').slick({
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.production-slider_bottom'
+    });
+    $('.production-slider_bottom').slick({
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<div class="slider-arrow slider-arrow_prod slider-arrow_left"><div>',
+        nextArrow: '<div class="slider-arrow slider-arrow_prod slider-arrow_right"><div>',
+        asNavFor: '.production-slider_top'
+    });
+});
