6bb083737d18c61b934168856709e082b114d200
andr0med0s <andromedoz@gmail.com>
2023-11-11 14:26:12 +0500

    barbershop_Float_added_comment_to_shadow_JS

--- akademy/Barbershop/barbershop_Float/js/shadow.js
+++ akademy/Barbershop/barbershop_Float/js/shadow.js
@@ -2,7 +2,24 @@
 var button = document.querySelectorAll ('.price-btn .btn');
 var block = document.querySelectorAll(".product");
 
+//----------ПЕРВОЕ---------------
+//применить свойство к нулевому индексу свойство
+// button[0].addEventListener("mouseenter", function() {
+//   block[0].classList.add("shadow");
+// });
 
+//----------ВТОРОЕ---------------
+//применить для каждого элемента с текущим индексом
+// button.forEach((button, index) => {
+//     button.addEventListener('mouseover', () => block[index].classList.add('product-shadow'));
+// });
+
+// button.forEach((button, index) => {
+//     button.addEventListener('mouseleave', () => block[index].classList.remove('product-shadow'));
+// });
+
+//----------ТРЕТЬЕ---------------
+//применить для каждого элемента свойство
 // button.forEach((button) => {
 //     button.addEventListener("mouseover", () => {
 //       block.forEach((block) => {
@@ -11,19 +28,41 @@ var block = document.querySelectorAll(".product");
 //     });
 //   });
 
+//----------ЧЕТВЕРТОЕ---------------
+//применить перебирая массив к конкретному индексу свойство
+// for (let i = 0; i < block.length; i++) {
+//     button[i].addEventListener('mouseover', () => {
+//     block[i].classList.add('product-shadow');
+//     });
+//     }
 
 
-button.forEach((button, index) => {
-    button.addEventListener('mouseover', () => block[index].classList.add('product-shadow'));
-});
+//----------ПЯТОЕ---------------
+//без использования стрелочной функции
+for (let i = 0; i < block.length; i++) {
+    button[i].addEventListener('mouseover', function() {
+    block[i].classList.add('product-shadow');
+    });
+    }
 
-button.forEach((button, index) => {
-    button.addEventListener('mouseleave', () => block[index].classList.remove('product-shadow'));
-});
+for (let j = 0; j < block.length; j++) {
+    button[j].addEventListener('mouseleave', function() {
+    block[j].classList.remove('product-shadow');
+    });
+    }
 
-// button[0].addEventListener("mouseenter", function() {
-//   block[0].classList.add("shadow");
-// });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
